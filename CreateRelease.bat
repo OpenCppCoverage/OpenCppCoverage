e7e4877ecbf57e66e9e52a5182dce1c3c99240c5
Olivier <Olivier@email.com>
2014-06-01 18:31:24 +0200

    Update CreateRelease

--- CreateRelease.bat
+++ CreateRelease.bat
@@ -1,24 +1,25 @@
 mkdir NewRelease
-mkdir NewRelease\Template
-xcopy /y /s /e Release\Template NewRelease\Template
-xcopy /y Release\OpenCppCoverage.exe NewRelease
-xcopy /y Release\Exporter.dll NewRelease
-xcopy /y Release\CppCoverage.dll NewRelease
-xcopy /y Release\Tools.dll NewRelease
-xcopy /y Release\libctemplate.dll NewRelease
-xcopy /y Release\template_test_util_test.exe NewRelease
-xcopy /y Release\boost_filesystem-vc120-mt-1_55.dll NewRelease
-xcopy /y Release\boost_program_options-vc120-mt-1_55.dll NewRelease
-xcopy /y Release\boost_system-vc120-mt-1_55.dll NewRelease
-xcopy /y Release\boost_log-vc120-mt-1_55.dll NewRelease
-xcopy /y Release\boost_thread-vc120-mt-1_55.dll NewRelease
-xcopy /y Release\boost_regex-vc120-mt-1_55.dll NewRelease
-xcopy /y Release\boost_date_time-vc120-mt-1_55.dll NewRelease
-xcopy /y Release\boost_locale-vc120-mt-1_55.dll NewRelease
-xcopy /y Release\boost_chrono-vc120-mt-1_55.dll NewRelease
+mkdir NewRelease\Binaries
+mkdir NewRelease\Binaries\Template
+xcopy /y /s /e Release\Template NewRelease\Binaries\Template
+xcopy /y Release\OpenCppCoverage.exe NewRelease\Binaries
+xcopy /y Release\Exporter.dll NewRelease\Binaries
+xcopy /y Release\CppCoverage.dll NewRelease\Binaries
+xcopy /y Release\Tools.dll NewRelease\Binaries
+xcopy /y Release\libctemplate.dll NewRelease\Binaries
+xcopy /y Release\template_test_util_test.exe NewRelease\Binaries
+xcopy /y Release\boost_filesystem-vc120-mt-1_55.dll NewRelease\Binaries
+xcopy /y Release\boost_program_options-vc120-mt-1_55.dll NewRelease\Binaries
+xcopy /y Release\boost_system-vc120-mt-1_55.dll NewRelease\Binaries
+xcopy /y Release\boost_log-vc120-mt-1_55.dll NewRelease\Binaries
+xcopy /y Release\boost_thread-vc120-mt-1_55.dll NewRelease\Binaries
+xcopy /y Release\boost_regex-vc120-mt-1_55.dll NewRelease\Binaries
+xcopy /y Release\boost_date_time-vc120-mt-1_55.dll NewRelease\Binaries
+xcopy /y Release\boost_locale-vc120-mt-1_55.dll NewRelease\Binaries
+xcopy /y Release\boost_chrono-vc120-mt-1_55.dll NewRelease\Binaries
 
-mkdir NewReleasePdb
-xcopy /y Release\OpenCppCoverage.pdb NewReleasePdb
-xcopy /y Release\Exporter.pdb NewReleasePdb
-xcopy /y Release\CppCoverage.pdb NewReleasePdb
-xcopy /y Release\Tools.pdb NewReleasePdb
+mkdir NewRelease\Pdb
+xcopy /y Release\OpenCppCoverage.pdb NewRelease\Pdb
+xcopy /y Release\Exporter.pdb NewRelease\Pdb
+xcopy /y Release\CppCoverage.pdb NewRelease\Pdb
+xcopy /y Release\Tools.pdb NewRelease\Pdb
