7b34177172dea4c4705a569842758029f9b50b9a
Olivier <Olivier@email.com>
2014-06-01 17:30:18 +0200

    Add Copyright to headers and sources.
    Remove targetver.h

--- CppCoverage/Wildcards.hpp
+++ CppCoverage/Wildcards.hpp
@@ -1,3 +1,19 @@
+// OpenCppCoverage is an open source code coverage for C++.
+// Copyright (C) 2014 OpenCppCoverage
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #pragma once
 
 #include <regex>
--- ExporterTest/stdafx.h
+++ ExporterTest/stdafx.h
@@ -1,14 +1,20 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
+// OpenCppCoverage is an open source code coverage for C++.
+// Copyright (C) 2014 OpenCppCoverage
 //
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "targetver.h"
-
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-
-
-// TODO: reference additional headers your program requires here
